6a3f4ab9ed7fed6bb0fd19fc60fc0b90784c5ae6
vtb7 <88791316+vtb7@users.noreply.github.com>
2022-03-19 13:35:47 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -56,7 +56,7 @@ function draw() {
 
   // kangaroo.x=camera.positionX-270;
   // kangaroo.x=Camera.position.x-270;
-  // kangaroo.x=camera.position.x-270;
+   kangaroo.x=camera.position.x-270;
   // kangaroo.x=Camera.Position.X-270;
    
   if (gameState===PLAY){
